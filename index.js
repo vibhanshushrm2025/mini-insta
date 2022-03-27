0e169e8e5c230f60d491b85624d3f571d45b9694
VibhanshuSharma <21je1039@iitism.ac.in>
2022-03-28 00:02:07 +0530

    more modification in mini insta

--- index.js
+++ index.js
@@ -8,15 +8,23 @@ firebase.auth().onAuthStateChanged((user) => {
   }
 });
 
-function signup() {
+  
+  
 
+
+function signup() {
+  const nname=document.getElementById("name").value
+  const username=document.getElementById("signupname").value
   const email = document.getElementById("signupemail").value
   const password = document.getElementById("signuppassword").value
   const confirmpassword = document.getElementById("signuppasswordconfirm").value
+  
   if (password == confirmpassword) {
+    firebase.database().ref("user1/username").set(username);   
+    firebase.database().ref("user1/name").set(nname);
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
-
+        
       })
       .catch((error) => {
         document.getElementById("error").innerHTML = error.message
---
+++ savedimages.js
@@ -0,0 +1,7 @@
+firebase.database().ref("user1/username").on("value", (snapshot) => {
+    document.getElementById("usrname").innerHTML = snapshot.val();
+});
+firebase.database().ref("user1/name").on("value", (snapshot) => {
+    document.getElementById("namee").innerHTML = snapshot.val();
+});
+
